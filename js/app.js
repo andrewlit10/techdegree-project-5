ccf531d66d8843d0f0921197b4d0c342aaa84e03
andrewlit10 <63842626+andrewlit10@users.noreply.github.com>
2021-04-23 15:04:57 -0700

    final

--- js/app.js
+++ js/app.js
@@ -1,3 +1,32 @@
 
 
+
+// Variables 
 baguetteBox.run('.gallery');
+let input = document.getElementById("search");
+let filter = input.value.toLowerCase();
+
+// Event Listeners 
+input.addEventListener("keyup", filterPhotos);
+input.addEventListener("keyup", lowercaseInput);
+
+function lowercaseInput() {
+  input.value = input.value.toLowerCase();
+}
+
+/* Search Function */
+
+function photoSearch() { 
+    let input = document.getElementById('search').value;
+    let a = document.getElementsByTagName('a');
+    input = input.toLowerCase(); 
+     
+    
+    for (i = 0; i < a.length; i++) { 
+        if (!a[i].getAttribute('data-caption').toLowerCase().includes(input)) { 
+            a[i].style.display="none"; 
+        } else { 
+            a[i].style.display="";				 
+            } 
+     } 
+} 
